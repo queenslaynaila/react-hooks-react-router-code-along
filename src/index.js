abbf61ce7872f43ab101eee3e66d6b0f34724bd3
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-10-01 12:24:04 +0300

    completed assignments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter,Routes,Route,NavLink} from "react-router-dom"
+import Navbar from "./Navbar";
+import Home from "./Components/Home"
+import About from "./Components/About"
+import Login from "./Components/Login"
+import Signup from "./Components/Signup"
+import Messages from "./Components/Messages"
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+ReactDOM.render(
+  <BrowserRouter>
+   <Navbar></Navbar>
+  <Routes>
+    <Route path="/" element={<Home></Home>}></Route>
+    <Route path="/about" element={<About></About>}></Route>
+    <Route path="/login" element={<Login></Login>}></Route>
+    <Route path="/signup" element={<Signup></Signup>}></Route>
+    <Route path="/messages" element={<Messages></Messages>}></Route>
+  </Routes>
+  </BrowserRouter>,
+
+ document.getElementById("root")
+ );
